190fbbf242991fa12e2d9bd96bcbe91538e7f66d
Saar <saarphone@gmail.com>
2023-01-22 14:54:39 +0200

    create main view

--- app/src/main/java/com/example/barbershophit/MainActivity.java
+++ app/src/main/java/com/example/barbershophit/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.barbershophit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+Button connectBtn,registerBtn;
+EditText email,password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        connectBtn = findViewById(R.id.btnConnect);
+        registerBtn=findViewById(R.id.btnGoToRegisterUser);
+        email=findViewById(R.id.editTextTextEmailAddress);
+        password=findViewById(R.id.editTextTextPassword);
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in1 = new Intent(MainActivity.this,UserRegister.class);
+                startActivity(in1);
+            }
+        });
+
+        connectBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println(email.getText().toString());
+                System.out.println(password.getText().toString());
+
+            }
+        });
     }
 }
