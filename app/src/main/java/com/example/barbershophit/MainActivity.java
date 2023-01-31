a6558b78a862a67ae1e3a72deb39629d0dee831c
Saar <saarphone@gmail.com>
2023-01-31 15:17:22 +0200

    Splash Screen

--- app/src/main/java/com/example/barbershophit/MainActivity.java
+++ app/src/main/java/com/example/barbershophit/MainActivity.java
@@ -1,28 +1,40 @@
 package com.example.barbershophit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+import android.window.SplashScreen;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 Button connectBtn,registerBtn;
 EditText email,password;
+private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
         connectBtn = findViewById(R.id.btnConnect);
-        registerBtn=findViewById(R.id.btnGoToRegisterUser);
-        email=findViewById(R.id.editTextTextEmailAddress);
-        password=findViewById(R.id.editTextTextPassword);
+        registerBtn = findViewById(R.id.btnGoToRegisterUser);
+        email = findViewById(R.id.editTextTextEmailAddress);
+        password = findViewById(R.id.editTextTextPassword);
         registerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent in1 = new Intent(MainActivity.this,UserRegister.class);
+                Intent in1 = new Intent(MainActivity.this, UserRegister.class);
                 startActivity(in1);
             }
         });
@@ -32,8 +44,13 @@ EditText email,password;
             public void onClick(View v) {
                 System.out.println(email.getText().toString());
                 System.out.println(password.getText().toString());
-
+                String cEmail = email.getText().toString();
+                String cPassword = password.getText().toString();
             }
+
         });
     }
+
 }
+
+
---
+++ app/src/main/java/com/example/barbershophit/Splash.java
@@ -0,0 +1,26 @@
+package com.example.barbershophit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash extends AppCompatActivity {
+    private final int SPLASH_DISPLAY_LENGTH = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(Splash.this,MainActivity.class);
+                Splash.this.startActivity(mainIntent);
+                //Splash.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
